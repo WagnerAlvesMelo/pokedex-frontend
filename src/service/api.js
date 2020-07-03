b1e438d51c545a3df4f5e715ae519b8979ff0f3e
WagnerAlvesMelo <walves13@outlook.com>
2020-07-02 23:03:39 -0300

    search and delete changes

--- src/service/api.js
+++ src/service/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "https://pokedexredfox.herokuapp.com/"
+  baseURL: process.env.NODE_ENV==="development" ? 'http://127.0.0.1:3333/' : "https://pokedexredfox.herokuapp.com/"
 });
-
 
 
 export default api;
